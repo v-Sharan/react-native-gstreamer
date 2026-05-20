15745afd580341222cba3da27ffac6a90b4bfbbb
v-Sharan <vsharansago@gmial.com>
2026-05-20 15:57:34 +0530

    new changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { requireNativeComponent, Text, View, UIManager, findNodeHandle, AppState, Platform } from 'react-native'
-
-const PropTypes = require('prop-types')
+import React, { useEffect, useRef, useCallback, forwardRef, useImperativeHandle } from 'react'
+import { requireNativeComponent, UIManager, findNodeHandle, AppState, Platform, ViewPropTypes } from 'react-native'
+import PropTypes from 'prop-types'
 
 export const GstState = {
     VOID_PENDING: 0,
@@ -11,156 +10,157 @@ export const GstState = {
     PLAYING: 4
 }
 
-export default class GstPlayer extends React.Component {
+const GstPlayer = forwardRef((props, ref) => {
+    const playerViewRef = useRef(null)
+    const playerHandleRef = useRef(null)
+    const appStateRef = useRef('active')
+    const currentGstStateRef = useRef(undefined)
+    const isInitializedRef = useRef(false)
 
-    currentGstState = undefined
-    appState = "active"
-    isInitialized = false
-
-    componentDidMount() {
-        this.playerHandle = findNodeHandle(this.playerViewRef)
-        AppState.addEventListener('change', this.appStateChanged)
-    }
-
-    componentWillUnmount() {
-        AppState.removeEventListener('change', this.appStateChanged)
-    }
-
-    appStateChanged = (nextAppState) => {
-        if (this.appState.match(/inactive|background/) && nextAppState === 'active') {
-
-            // On iOS we need to recreate video sink to bypass a bug with vtdec when video freezes
-            if (Platform.OS === 'ios') {
-                this.recreateView()
-            }
-            this.play()
-        } else {
-            this.stop()
-        }
-        this.appState = nextAppState
-    }
-
-    // Callbacks
-    onPlayerInit() {
-        this.isInitialized = true
-
-        if (this.props.onPlayerInit)
-            this.props.onPlayerInit()
-    }
-
-    onStateChanged(_message) {
-        const { old_state, new_state } = _message.nativeEvent
-        this.currentGstState = new_state
-
-        if (old_state === GstState.PAUSED && new_state === GstState.READY) {
-
-            // On iOS we need to recreate video sink to bypass a bug with vtdec when video freezes
-            if (Platform.OS === 'ios')
-                this.recreateView()
+    const setGstState = useCallback((state) => {
+        const handle = playerHandleRef.current
+        if (!handle) {
+            return
         }
 
-        if (this.props.onStateChanged)
-            this.props.onStateChanged(old_state, new_state)
-    }
-
-    onVolumeChanged(_message) {
-        const { rms, peak, decay } = _message.nativeEvent
-
-        if (this.props.onVolumeChanged)
-            this.props.onVolumeChanged(rms, peak, decay)
-    }
-
-    onUriChanged(_message) {
-        const { new_uri } = _message.nativeEvent
-
-        if (this.props.onUriChanged) {
-            this.props.onUriChanged(new_uri)
-        }
-
-        if (this.props.autoPlay) {
-            this.play()
-        }
-    }
-
-    onEOS() {
-        if (this.props.onEOS)
-            this.props.onEOS()
-    }
-
-    onElementError(_message) {
-        const { source, message, debug_info } = _message.nativeEvent
-
-        if (this.props.onElementError)
-            this.props.onElementError(source, message, debug_info)
-    }
-
-    shouldComponentUpdate() {
-        return true
-    }
-
-    // Methods
-    setGstState(state) {
         UIManager.dispatchViewManagerCommand(
-            this.playerHandle,
+            handle,
             UIManager.RCTGstPlayer.Commands.setState,
             [state]
         )
-    }
+    }, [])
 
-    // Player state shortcuts
-    play() {
-        this.setGstState(GstState.PLAYING)
-    }
+    const recreateView = useCallback(() => {
+        const handle = playerHandleRef.current
+        if (!handle) {
+            return
+        }
 
-    pause() {
-        this.setGstState(GstState.PAUSED)
-    }
-
-    stop() {
-        this.setGstState(GstState.READY)
-    }
-
-    // Helper methods
-    recreateView() {
         UIManager.dispatchViewManagerCommand(
-            this.playerHandle,
+            handle,
             UIManager.RCTGstPlayer.Commands.recreateView,
             []
         )
-    }
+    }, [])
 
-    render() {
-        return (
-            <RCTGstPlayer
-                autoPlay={this.props.autoPlay}
-                uri={this.props.uri || undefined}
-                audioLevelRefreshRate={this.props.audioLevelRefreshRate !== undefined ? this.props.audioLevelRefreshRate : 100}
-                isDebugging={this.props.isDebugging !== undefined ? this.props.isDebugging : false}
+    const play = useCallback(() => setGstState(GstState.PLAYING), [setGstState])
+    const pause = useCallback(() => setGstState(GstState.PAUSED), [setGstState])
+    const stop = useCallback(() => setGstState(GstState.READY), [setGstState])
 
-                onPlayerInit={this.onPlayerInit.bind(this)}
-                onStateChanged={this.onStateChanged.bind(this)}
-                onVolumeChanged={this.onVolumeChanged.bind(this)}
-                onUriChanged={this.onUriChanged.bind(this)}
-                onEOS={this.onEOS.bind(this)}
-                onElementError={this.onElementError.bind(this)}
+    const appStateChanged = useCallback((nextAppState) => {
+        if (appStateRef.current.match(/inactive|background/) && nextAppState === 'active') {
+            if (Platform.OS === 'ios') {
+                recreateView()
+            }
+            play()
+        } else {
+            stop()
+        }
+        appStateRef.current = nextAppState
+    }, [play, stop, recreateView])
 
-                ref={(playerView) => this.playerViewRef = playerView}
+    const onPlayerInit = useCallback(() => {
+        isInitializedRef.current = true
+        if (props.onPlayerInit) {
+            props.onPlayerInit()
+        }
+    }, [props.onPlayerInit])
 
-                {...this.props}
-            />
-        )
-    }
-}
+    const onStateChanged = useCallback((_message) => {
+        const { old_state, new_state } = _message.nativeEvent
+        currentGstStateRef.current = new_state
+
+        if (old_state === GstState.PAUSED && new_state === GstState.READY) {
+            if (Platform.OS === 'ios') {
+                recreateView()
+            }
+        }
+
+        if (props.onStateChanged) {
+            props.onStateChanged(old_state, new_state)
+        }
+    }, [props.onStateChanged, recreateView])
+
+    const onVolumeChanged = useCallback((_message) => {
+        const { rms, peak, decay } = _message.nativeEvent
+        if (props.onVolumeChanged) {
+            props.onVolumeChanged(rms, peak, decay)
+        }
+    }, [props.onVolumeChanged])
+
+    const onUriChanged = useCallback((_message) => {
+        const { new_uri } = _message.nativeEvent
+
+        if (props.onUriChanged) {
+            props.onUriChanged(new_uri)
+        }
+
+        if (props.autoPlay) {
+            play()
+        }
+    }, [props.onUriChanged, props.autoPlay, play])
+
+    const onEOS = useCallback(() => {
+        if (props.onEOS) {
+            props.onEOS()
+        }
+    }, [props.onEOS])
+
+    const onElementError = useCallback((_message) => {
+        const { source, message, debug_info } = _message.nativeEvent
+        if (props.onElementError) {
+            props.onElementError(source, message, debug_info)
+        }
+    }, [props.onElementError])
+
+    useImperativeHandle(ref, () => ({
+        setGstState,
+        play,
+        pause,
+        stop,
+        recreateView
+    }), [setGstState, play, pause, stop, recreateView])
+
+    useEffect(() => {
+        playerHandleRef.current = findNodeHandle(playerViewRef.current)
+
+        const subscription = AppState.addEventListener?.('change', appStateChanged)
+
+        return () => {
+            if (subscription && typeof subscription.remove === 'function') {
+                subscription.remove()
+            } else {
+                AppState.removeEventListener('change', appStateChanged)
+            }
+        }
+    }, [appStateChanged])
+
+    return (
+        <RCTGstPlayer
+            autoPlay={props.autoPlay}
+            uri={props.uri || undefined}
+            audioLevelRefreshRate={props.audioLevelRefreshRate !== undefined ? props.audioLevelRefreshRate : 100}
+            isDebugging={props.isDebugging !== undefined ? props.isDebugging : false}
+
+            onPlayerInit={onPlayerInit}
+            onStateChanged={onStateChanged}
+            onVolumeChanged={onVolumeChanged}
+            onUriChanged={onUriChanged}
+            onEOS={onEOS}
+            onElementError={onElementError}
+
+            ref={playerViewRef}
+            {...props}
+        />
+    )
+})
 
 GstPlayer.propTypes = {
-
-    // Props
     uri: PropTypes.string,
     autoPlay: PropTypes.bool,
     audioLevelRefreshRate: PropTypes.number,
     isDebugging: PropTypes.bool,
 
-    // Events callbacks
     onPlayerInit: PropTypes.func,
     onStateChanged: PropTypes.func,
     onVolumeChanged: PropTypes.func,
@@ -168,19 +168,19 @@ GstPlayer.propTypes = {
     onEOS: PropTypes.func,
     onElementError: PropTypes.func,
 
-    // Methods
     setGstState: PropTypes.func,
     play: PropTypes.func,
     pause: PropTypes.func,
     stop: PropTypes.func,
 
-    // Helper methods
     createDrawableSurface: PropTypes.func,
     destroyDrawableSurface: PropTypes.func,
     recreateView: PropTypes.func,
 
-    ...View.propTypes
+    ...ViewPropTypes
 }
+
+export default GstPlayer
 
 const RCTGstPlayer = requireNativeComponent('RCTGstPlayer', GstPlayer, {
     nativeOnly: { onChange: true }
